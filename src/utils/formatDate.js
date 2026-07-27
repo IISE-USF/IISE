8a5f62a5893c27dcc8127eaecc45d2652ef16646
denisstaruschenko-cmd <denis.staruschenko@gmail.com>
2026-07-27 15:54:55 -0400

    Update formatDate.js

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,1 +1,11 @@
 
+export function formatDate(dateStr) {
+  if (!dateStr) return "";
+  const [year, month, day] = dateStr.split("-").map(Number);
+  const date = new Date(year, month - 1, day);
+  return date.toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+}
