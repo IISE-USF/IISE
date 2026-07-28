2c675c5aa48c4e1fcaa39688f200c1cb35c66817
denisstaruschenko-cmd <denis.staruschenko@gmail.com>
2026-07-28 18:03:31 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
